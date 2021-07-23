5327d4f40629f61d8c8912a5b3d658eb81fb1986
Vishal Pratap Singh <vishal.pratapsingh@zeotap.com>
2021-07-23 20:32:02 +0530

    test: added faliure tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,36 +1,39 @@
 package org.example;
 
-public class App 
-{
+import lombok.Data;
+
+@Data
+public class App {
 
     DummyInterface dummy;
 
-    public App(DummyInterface dummy){
+    public App(DummyInterface dummy) {
         this.dummy = dummy;
     }
 
-    public int func1(int num1, int num2){
-        return add(num1,num2)+dummy.func2(num1,num2);
+    public Integer func1(Integer num1, Integer num2) {
+        return add(num1, num2) + dummy.func2(num1, num2);
     }
 
-    public int add(int num1, int num2){
-        return num1+num2;
+    public Integer add(Integer num1, Integer num2) {
+        return num1 + num2;
     }
 
-    public int sub(int num1, int num2){
-        return num1-num2;
+    public Integer sub(Integer num1, Integer num2) {
+        return num1 - num2;
     }
 
-    public int mul(int num1, int num2){
-        return num1*num2;
+    public Integer mul(Integer num1, Integer num2) {
+        return num1 * num2;}
+
+    public Integer div(Integer num1, Integer num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero!");
+        }
+        return num1 / num2;
     }
 
-    public int div(int num1, int num2){
-        return num1/num2;
-    }
+    public static void main(String[] args) {
 
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
     }
 }
